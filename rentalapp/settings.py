aa9b7c46b2fec2a2efbd2db6b4ee85375c224d91
Alfie <alfredokang@gmail.com>
2020-01-20 10:25:58 -0500

    Update settings.py

--- rentalapp/settings.py
+++ rentalapp/settings.py
@@ -11,9 +11,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-with open('~/secret_key.txt') as f:
-    SECRET_KEY = f.read().strip()
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
