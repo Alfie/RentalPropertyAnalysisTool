2ee9806bae4433799107217a0dc25dab6067d714
Alfie <alfredokang@gmail.com>
2020-01-23 12:43:59 -0500

    Update urls.py

--- propertyanalysis/urls.py
+++ propertyanalysis/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('calc/', views.calc, name='calc')
+    path('', views.index, name='index')
 ]
