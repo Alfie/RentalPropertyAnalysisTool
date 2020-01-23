f830457e858552e88f93eda661367a37507245d6
Alfie <alfredokang@gmail.com>
2020-01-23 04:36:03 -0500

    Update models.py
    create a data model for the income portion of the rental analysis app

--- propertyanalysis/models.py
+++ propertyanalysis/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+Class Property(models.Model):
+  rental_income = models.FloatField(default=0)
+  laundry_income = models.FloatField(default=0)
+  storage_income = models.FloatField(default=0)
+  misc_income = models.FloatField(default=0)
+  
