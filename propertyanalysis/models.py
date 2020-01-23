9d80167552424d22b7fbb16c55d71b11103d7be0
Alfie <alfredokang@gmail.com>
2020-01-23 07:16:50 -0500

    typo

--- propertyanalysis/models.py
+++ propertyanalysis/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-Class Property(models.Model):
+class Property(models.Model):
   rental_income = models.FloatField(default=0)
   laundry_income = models.FloatField(default=0)
   storage_income = models.FloatField(default=0)
