60dbb20f7738cd9fd709e1cbe5cedf0c173622bf
Alfie <alfredokang@gmail.com>
2020-01-23 07:10:25 -0500

    test post fields on index page

--- propertyanalysis/views.py
+++ propertyanalysis/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404,render
+from django.urls import reverse
+from django.views import generic
 
 def index(request):
-    return render(request, 'propertyanalysis/index.html')
+    test = request.POST['rincome']
+    context = {'test' : test}
+    return render(request, 'propertyanalysis/index.html', context)
