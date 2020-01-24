4e408c5887a8d08d6c47d8e9b7188cd9d2033882
Alfie <alfredokang@gmail.com>
2020-01-24 11:39:04 -0500

    Update settings.py

--- rentalapp/settings.py
+++ rentalapp/settings.py
@@ -3,6 +3,7 @@ Django settings for rentalapp project.
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
