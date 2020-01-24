eeaf2bda969e5737b06a7a9f7786e1fa381229ea
Alfie <alfredokang@gmail.com>
2020-01-23 22:34:25 -0500

    Update views.py

--- propertyanalysis/views.py
+++ propertyanalysis/views.py
@@ -5,8 +5,8 @@ from django.views import generic
 
 from . models import Property
 
-property1 = Property(rental_income=7)
-property1.save()
+#property1 = Property(rental_income=7)
+#property1.save()
 
 def index(request):
     return render(request, 'propertyanalysis/index.html')
