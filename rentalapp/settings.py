67e24a163e24a4a8b1fb3cdc3f43d2a493d1d908
Alfie <alfredokang@gmail.com>
2020-01-24 11:42:19 -0500

    Update settings.py

--- rentalapp/settings.py
+++ rentalapp/settings.py
@@ -64,12 +64,8 @@ WSGI_APPLICATION = 'rentalapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 
 # Password validation
