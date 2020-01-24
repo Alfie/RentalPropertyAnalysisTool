8831ce879e54106c005527e9bbc2085c1d75b430
Alfie <alfredokang@gmail.com>
2020-01-24 14:45:03 -0500

    Update views.py

--- propertyanalysis/views.py
+++ propertyanalysis/views.py
@@ -9,6 +9,11 @@ property1 = Property(rental_income=7)
 property1.save()
 
 def index(request):
+    test = Property.objects.all()
+    context = {'test': test}
+    return render(request, 'propertyanalysis/index.html', context)
+
+def edit(request)
     p2 = Property(rental_income=request.POST['rincome'])
     p2.save()
     test = Property.objects.all()
