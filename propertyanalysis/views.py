f10c731c0aacff2a08c0ffcab3886d852d3a15c0
Alfie <alfredokang@gmail.com>
2020-01-23 22:23:22 -0500

    test if data migration worked

--- propertyanalysis/views.py
+++ propertyanalysis/views.py
@@ -6,6 +6,6 @@ from django.views import generic
 from . models import Property
 
 def index(request):
-    #property1 = Property(rental_income=7)
-    #property1.save()
+    property1 = Property(rental_income=7)
+    property1.save()
     return render(request, 'propertyanalysis/index.html')
