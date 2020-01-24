bcde753adf927fc64ac4387664d53058e5b4662d
Alfie <alfredokang@gmail.com>
2020-01-24 12:12:07 -0500

    Update views.py

--- propertyanalysis/views.py
+++ propertyanalysis/views.py
@@ -3,10 +3,10 @@ from django.shortcuts import get_object_or_404,render
 from django.urls import reverse
 from django.views import generic
 
-from . models import Property
+from .models import Property
 
-#property1 = Property(rental_income=7)
-#property1.save()
+property1 = Property(rental_income=7)
+property1.save()
 
 def index(request):
     return render(request, 'propertyanalysis/index.html')
